8ed2e3a2c7f7becb75b045841ba218c813a5f848
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2021-08-18 12:00:34 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -17,7 +17,7 @@ function send() {
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
 
-    actual_answer = parasInt(number1) * parasInt(number2);
+    actual_answer = parseInt(number1)*parseInt(number2);
 
     question = "<h4>" + number1 + " X " + number2 + "</h4>";
     input = "<br> Answer : <input type='text' id='input_checkbox'> "
@@ -27,7 +27,7 @@ function send() {
     document.getElementById("output").innerHTML = row;
 
     document.getElementById("number1").value = "";
-    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
 }
 
 
